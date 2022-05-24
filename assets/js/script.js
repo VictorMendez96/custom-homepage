6e25c2a868e1b64dad67a81100ba0fcb715e80b9
Mlenaya <anaya6226@gmail.com>
2022-05-24 17:12:32 -0600

    Connect OpenWeatherAMap API

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,12 @@ async function getImages(url){
  console.log(data)
 }
 getImages(URL)
+const WETHER_API_KEY="28d5ff15f412925443d50a7b9ee6b8ce"
+const WEATHER_URL=``
+async function getWeatherData(url){
+    const response=await fetch(url)
+    const data=await response.json()
+    console.log(data)
+
+   }
+   getWeatherData(WEATHER_URL)
