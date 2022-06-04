53f2f4fc1b53038ad2bd4ffd35904ad57f769486
Brandon E <brandonelliott0594@gmail.com>
2022-06-04 13:40:17 -0600

    small changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 let userSettings = {};
 let musicVar = 0;let redditVar = 0;let locVar = 0;let remVar =0;
 let redditHolder = document.getElementById("reddit");
-
+console.log(userSettings);
 let setPage = () => {
     getImages(URL)
     fetchPosts()
@@ -19,78 +19,22 @@ async function getImages(url){
 
 
 
-function fetchPosts() {
-var popular = "https://www.reddit.com/r/popular/hot.json"
-    fetch(popular)
-        .then( function (response) {
-            return response.json()
-        })
-        .then(function (object) {
-            console.log(object)
-            
-            for (let i=0; i < 25; i++) {
-                var urlSrc = object.data.children[i].data.url
-                var title = object.data.children[i].data.title
-                // Creates card contianer and sets card attribute
-                // ================================================
-                var contentCard = document.createElement("div")
-                contentCard.classList.add("card")                        
-                // creates image div and attributes
-                // ================================
-                var cardImgEl = document.createElement("div")
-                cardImgEl.classList.add("card-image")
-                var thumbnail = document.createElement("img")
-                thumbnail.classList.add("card-image")
-                thumbnail.setAttribute("src", object.data.children[i].data.thumbnail)
-                thumbnail.setAttribute("alt", "Post Thumbnail")
-                // sets different thumbnails for different types of posts
-                // ================================
-                if(object.data.children[i].data.thumbnail === "self" || object.data.children[i].data.thumbnail === "default") {
-                    thumbnail.setAttribute("src", "/custom-homepage/assets/img/reddit_logo_horizontal_on_orangered.png")
-                } else if(object.data.children[i].data.thumbnail === "nsfw") {
-                    thumbnail.setAttribute("src", "/custom-homepage/assets/img/interstitial-image-over18.png")
-                    thumbnail.style.height = ("160px")
-                } else {
-                    thumbnail.style.height = ("200px")
-                }
-                // sets post title to the card content
-                // ================================
-                var cardContent = document.createElement("div")
-                cardContent.classList.add("card-content")
-                cardContent.style.fontSize = "20px"
-                // cardContent.style.fontWeight = "bold"
-                // Appends items to the Card
-                // ================================================
-                // card content
-                cardImgEl.append(thumbnail)
-                cardContent.append(title)
-                contentCard.append(cardContent)
-                contentCard.append(thumbnail)
-                redditHolder.append(contentCard) 
-            } 
-        })
 
-    }  
-     
-    // image.src = data.childeren[i].data.url_overridden_by_dest
-    // h4.textContent = body.data.children[i].data.title
-    // div.appendChild(h4)
-    // div.appendChild(image)
-    // parentdiv.appendChild(div)
 
 
 // Begin reddit API
 // =================================================================
-    // TODO: Make cards clickable
+    // TODO: 
         // - make the user selection the subreddit that is pulled
         // - butify the card holder and contents
         // - exempt NSFW posts
+        // - header
         
         function fetchPosts() {
-            var popular = "https://www.reddit.com/r/popular/hot.json"
+            var subreddit = `https://www.reddit.com/r/${redditVar}/hot.json`
             
             
-            fetch(popular)
+            fetch(subreddit)
             .then( function (response) {
                 return response.json()
             })
@@ -105,7 +49,7 @@ var popular = "https://www.reddit.com/r/popular/hot.json"
                     // Creates card contianer and sets card attribute
                     // ================================================
                     var contentCard = document.createElement("div")
-                    contentCard.classList.add("card")
+                    contentCard.classList.add("reddit-card")
                     contentCard.addEventListener("click", function (e) {
                         const eventTarget = e.currentTarget.firstChild.textContent
                         window.open(eventTarget)
@@ -124,9 +68,9 @@ var popular = "https://www.reddit.com/r/popular/hot.json"
             // sets different thumbnails for different types of posts
             // ================================
             if(object.data.children[i].data.thumbnail === "self" || object.data.children[i].data.thumbnail === "default") {
-                thumbnail.setAttribute("src", "/custom-homepage/assets/img/reddit_logo_horizontal_on_orangered.png")
+                thumbnail.setAttribute("src", ".assets/custom-homepage/assets/img/reddit_logo_horizontal_on_orangered.png")
             } else if(object.data.children[i].data.thumbnail === "nsfw") {
-                thumbnail.setAttribute("src", "/custom-homepage/assets/img/interstitial-image-over18.png")
+                thumbnail.setAttribute("src", ".assets/custom-homepage/assets/img/interstitial-image-over18.png")
                 thumbnail.style.height = ("160px")
             } else {
                 thumbnail.style.height = ("200px")
@@ -147,6 +91,7 @@ var popular = "https://www.reddit.com/r/popular/hot.json"
             cardTitle.textContent = subreddit
             
             // creates anchor tag for link
+            // ================================
             var a = document.createElement("a")
             var link = document.createTextNode(urlSrc)
             a.setAttribute("class", "hide")
@@ -162,31 +107,14 @@ var popular = "https://www.reddit.com/r/popular/hot.json"
             cardContent.append(title)
             contentCard.append(cardContent)
             contentCard.append(thumbnail)
-            
-           
-            
-            
             document.getElementById("reddit").append(contentCard)
             
-            
         } })
-        
-        
-        
-        
-                
-                
-                
-                
-           
-    
-          
     }
-    
     fetchPosts()
-   
-
-// Begin 1st opening function
+     
+    
+    // Begin 1st opening function
 function checkCustom() {
     userSettings = JSON.parse(localStorage.getItem("userSettings"));
     if (userSettings.remember != false) {
@@ -262,25 +190,5 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log(elems)
   });
 
-
-  //document.getElementsByClassName("btn modal-close").addEventListener("click",function() {
-  var iheartplayer = document.getElementById('iheart');
-  var station = document.createElement('iframe');
-  station.width = "100%";
-  station.height = "200";
-  station.frameborder = "0";
-  station.src = "https://www.iheart.com/live/american-top-40-4802/?embed=true";
-
-  //if (MusicVar == 0) {
-  //  station.src = "https://www.iheart.com/live/american-top-40-4802/?embed=true";
-  //} else {
-  //  station.src = "https://www.iheart.com/live/american-top-40-4802/?embed=true";
-  //}
-
-  
-  iheartplayer.appendChild(station);
-//})
-
   checkCustom()
-
 
